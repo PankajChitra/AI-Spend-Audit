54fd5ee80b282cb02ec1483c4f2f7efe56bd3359
Pankaj Chitra <pankajchitra17@gmail.com>
2026-05-12 22:28:47 +0530

    leadaudit fix

--- server/server.js
+++ server/server.js
@@ -22,6 +22,7 @@ app.use("/api/audit", auditRoutes);
 app.use("/api/leads", leadRoutes);
 const PORT = process.env.PORT || 5000;
 
+
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}`);
 });
